558112dee6ec6a7fcba626fb5c9b812618023fbd
irislee <irislee513@gmail.com>
2013-12-02 14:58:04 -0500

    made the game work

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,5 +1,26 @@
-$( document ).ready(function() {
-    $( ".js-img" ).click(function(){
-      alert("you clicked a thing");
-    });
-});
+scorer = function(element){
+  var hidden_field = $("#sound_type").attr("value");
+  var guess = $(element).attr("id");
+  if (hidden_field === guess){
+    show_results("You Win");
+  }
+  else {
+    show_results("You Lose");
+  }
+};
+
+show_results = function(message){
+  $("#result").html(message);
+  $("#again").show();
+};
+
+ready = function() {
+  $( ".js-img" ).on("click", function(){
+    scorer(this);
+  });
+};
+
+// document ready call the ready function
+$(document).ready(ready);
+// this is a dumb thing like document ready when you have turbolinks
+$(document).on('page:load', ready);
--- app/models/meow.rb
+++ app/models/meow.rb
@@ -1,2 +1,9 @@
 class Meow < ActiveRecord::Base
+  def sound_type
+    if self.cat
+      "cat"
+    else
+      "human"
+    end
+  end
 end
