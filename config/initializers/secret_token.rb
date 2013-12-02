dc6fbd7bc2d976aa04f24cc3bc3334eeaea94be7
irislee <irislee513@gmail.com>
2013-12-02 12:26:01 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HumanOrCat::Application.config.secret_key_base = 'b7a0d3a4959a5863fbfa78ef5e9ab82e2d153b1bef24f197feb92c5632832d9f72506ac2de3962e38c1002ca49c995431d865a16220c8fe7b6a8a6b0f2694065'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HumanOrCat::Application.config.session_store :cookie_store, key: '_human-or-cat_session'
