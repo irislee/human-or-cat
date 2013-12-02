676b0d1518828844607fc6ea5ff29f6bd0678bc9
irislee <irislee513@gmail.com>
2013-12-02 13:17:58 -0500

    randomized meows

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,7 +1,8 @@
 class GameController < ApplicationController
   def play
-    @sound = 
-    @image1 = '/human1.jpg'
+    @sound = Meow.order("RANDOM()").first
+    # @image1 = '/human1.jpg'
     # @image2 = cat
   end
 end
+
